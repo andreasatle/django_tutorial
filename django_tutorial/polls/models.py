65d4bacf38b8787b0357c92cffc619d0102ef453
Andreas Atle <atle.andreas@gmail.com>
2023-12-01 11:04:10 -0600

    Add the polls model.

--- django_tutorial/polls/models.py
+++ django_tutorial/polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class WithTimestamp(models.Model):
+    created_at = models.DateTimeField("date published", auto_now_add=True)
+    updated_at = models.DateTimeField("date of last change", auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Question(WithTimestamp):
+    question_text = models.CharField(max_length=200)
+
+class Choice(WithTimestamp):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
