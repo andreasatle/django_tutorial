6dacbf6e526837799532a69f12e3fef993e91387
Andreas Atle <atle.andreas@gmail.com>
2023-12-02 20:48:11 -0600

    Play a bit with admin.

--- django_tutorial/polls/admin.py
+++ django_tutorial/polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Question, Choice, WithTimestamp
 
-# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
