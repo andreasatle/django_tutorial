24053fe1a4601ef0de39069d7660b5f47cafc473
Andreas Atle <atle.andreas@gmail.com>
2023-12-03 09:17:07 -0600

    Add some more views, with urls.

--- django_tutorial/polls/views.py
+++ django_tutorial/polls/views.py
@@ -5,3 +5,24 @@ def index(request):
     This view function returns a HttpResponse with a greeting message.
     """
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    """
+    View function for displaying the details of a specific question.
+    """
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    """
+    View function for displaying the results of a specific question.
+    """
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+
+def vote(request, question_id):
+    """
+    Casts a vote for a specific question.
+    """
+    return HttpResponse(f"You're voting on question {question_id}.")
