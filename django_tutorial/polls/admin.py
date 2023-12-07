b3e34975be33bc08acae273e0550349face85aeb
Andreas Atle <atle.andreas@gmail.com>
2023-12-06 19:06:13 -0600

    Extend the admin forms.

--- django_tutorial/polls/admin.py
+++ django_tutorial/polls/admin.py
@@ -3,11 +3,14 @@ from .models import Question, Choice
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
+    readonly_fields = ['created_at', 'updated_at']
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'created_at', 'updated_at')
     fieldsets = [
         (None, {'fields': ['question_text']}),
     ]
     inlines = [ChoiceInline]
+    readonly_fields = ['created_at', 'updated_at']
 
 admin.site.register(Question, QuestionAdmin)
